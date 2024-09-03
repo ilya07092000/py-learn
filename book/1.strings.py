b7e00055540dcd125f9570aea274f81f237ea16e
ilya07092000 <ischenko2012@gmail.com>
2024-09-03 22:35:09 +0300

    added strings

---
+++ book/1.strings.py
@@ -0,0 +1,15 @@
+# f string
+name = 'Ilya'
+surname = 'Ischenko'
+# print(f"My name is {name}, my surname is {surname}")
+
+# changing a case
+# print('string for test'.title())
+# print('string for test'.capitalize())
+# print('string for test'.lower())
+# print('string for test'.upper())
+
+# remove whitespaces
+# print('   test  '.lstrip()) # left side
+# print('   test  '.rstrip()) # right side 
+# print('   test  '.strip()) # both sides
